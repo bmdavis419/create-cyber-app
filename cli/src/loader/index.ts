5c302f1846a104ecf33964803f2c90a73b419a4e
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-10-15 00:38:52 -0400

    ignore fix

--- cli/src/loader/index.ts
+++ cli/src/loader/index.ts
@@ -1,11 +1,13 @@
 // this is responsible for taking the templates and loading them onto the user's PC
 import { loadDirectory } from "../utils/loadDirectory";
+import path from "path";
+import fs from "fs-extra";
 
 const SOURCE_DIR = "templates/base";
 const BASE_ITEMS = [
   "src",
   "static",
-  ".gitignore",
+  "_gitignore",
   "package.json",
   "svelte.config.js",
   "tsconfig.json",
@@ -18,4 +20,8 @@ export const loadBaseTemplate = async (
   items = BASE_ITEMS
 ) => {
   await loadDirectory(sourceDir, projectDir, items);
+  fs.renameSync(
+    path.join(projectDir, "_gitignore"),
+    path.join(projectDir, ".gitignore")
+  );
 };
